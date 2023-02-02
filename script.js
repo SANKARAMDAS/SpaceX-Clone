c1060f94510a061221b6339a3eb06535047bec11
DSankaram <sankaram@octaloop.com>
2023-02-02 14:50:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const btn = document.getElementById('menu-btn');
+const overlay = document.getElementById('overlay');
+const menu = document.getElementById('mobile-menu');
+const counters = document.getElementById('.counter');
+
+btn.addEventListener('click', navToggle);
+
+function navToggle(){
+    btn.classList.toggle('open');
+    overlay.classList.toggle('overlay-show');
+    document.body.classList.toggle('stop-scrolling');
+    menu.classList.toggle('show-menu');
+}
+
+function countUp() {
+    counters.forEach((counter) => {
+        counter.innerText = '0';
+
+        const updateCounter = () => {
+            //Get count target
+            const target = +counter.getAttribute('data-target');
+            //Get current counter value
+            const c = +counter.innerText;
+
+            //Create an increment
+            const increment = target / 100;
+
+            //If counter is less than target, add increment
+            if(c < target){
+                //Round up and set counter value
+                counter.innerText = `${Math.ceil(c + increment)}`;
+
+                setTimeOut(updateCounter, 75);
+            }
+            console.log(target);
+        }
+        updateCounter();
+    });
+}
+
+countUp();
